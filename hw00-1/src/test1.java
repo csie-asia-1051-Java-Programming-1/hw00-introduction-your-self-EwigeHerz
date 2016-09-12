53058516b3c05c36338175260c532a17929c1ccd
EwigeHerz <105021027@live.asia.edu.tw>
2016-09-12 15:50:25 +0800

    hw01

---
+++ hw00-1/src/test1.java
@@ -0,0 +1,11 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("名字：陳宴湘");
+       System.out.println("學號：105021027");
+       System.out.println("系別：資訊工程系數位內容組");
+	}
+
+}
